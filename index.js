af25d96ccff296ab24d537982bdf91c6cf85bcea
megan-lab <57483061+megan-lab@users.noreply.github.com>
2020-03-05 15:53:19 +0800

    Challenge part 1

---
+++ index.js
@@ -0,0 +1,114 @@
+const electron = require("electron");
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+let todayWindow;
+let createWindow;
+let listWindow;
+
+app.on("ready", () => {
+    todayWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true 
+        },
+        title: "Aplikasi Sewa Kebaya"
+    });
+    todayWindow.loadURL(`file://${__dirname}/today.html`);
+    todayWindow.on("closed", () => {
+
+        app.quit();
+        todayWindow = null;
+    });
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
+});
+
+const listWindowCreator = () => {
+    listWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration : true
+        },
+        width : 600,
+        height : 400,
+        title: "All Apponitment"
+    });
+    listWindow.setMenu(null);
+    listWindow.loadURL(`file://${__dirname}/list.html`);
+    listWindow.on("closed",() => (listWindow = null));
+};
+
+const createWindowCreator = () => {
+    createWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration : true
+        },
+        width : 600,
+        height : 400,
+        title: "Create Appointment"
+    });
+    createWindow.setMenu(null);
+    createWindow.loadURL(`file://${__dirname}/create.html`);
+    createWindow.on("closed",() => (createWindow = null));
+};
+
+const aboutWindowCreator = () => {
+    aboutWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration : true
+        },
+        width : 600,
+        height : 400,
+        title: "About Pages"
+    });
+    aboutWindow.setMenu(null);
+    aboutWindow.loadURL(`file://${__dirname}/about.html`);
+    aboutWindow.on("closed",() => (aboutWindow = null));
+};
+
+
+ipcMain.on("appointment:create", (event, appointment) => {
+    console.log(appointment);
+});
+
+const menuTemplate = [{
+    label: "File",
+    submenu: [{
+                label : "Create Appointment",
+               
+                click(){
+                    createWindowCreator();
+                }
+            },
+            {
+                label : "List Appointment",
+               
+                click(){
+                    listWindowCreator();
+                }
+            },
+
+            {
+                label: 'Quit',
+                accelerator : process.platform === "darwin" ? "Command + Q" : " Ctrl + Q",
+                click(){
+                    app.quit();
+                }
+            }
+        
+    ]
+},
+{
+    label: "View",
+    submenu: [{ role:"realod"},{role:"toogledevtools"}]
+},
+{
+    label: "About",
+        click(){
+            aboutWindowCreator();
+        }
+}
+
+
+]
